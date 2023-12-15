782977350980bae8864851f68732ad700a4bc064
GuRgEnA <agurg21@freeuni.edu.ge>
2023-12-15 18:08:37 +0400

    Small changes

--- safe-drive-back/src/main.ts
+++ safe-drive-back/src/main.ts
@@ -1,10 +1,15 @@
-const axios = require('axios');
+import {AxiosResponse} from "axios";
+
+import axios from "axios";
+
+const tbilisi: string = "41.6938,44.8015;"
+const batumi: string = '41.6461,41.6405;'
 
 const options = {
     method: 'GET',
     url: 'https://trueway-directions2.p.rapidapi.com/FindDrivingRoute',
     params: {
-        stops: '41.6938,44.8015;41.6461,41.6405;'
+        stops: tbilisi + batumi
     },
     headers: {
         'X-RapidAPI-Key': 'e3fc70ca95msh0c1271a45bc037fp13f1eajsn7c9aa6fa0095',
@@ -12,12 +17,11 @@ const options = {
     }
 };
 
-try {
-    axios.request(options)
-        .then((response: any) =>{
-            console.log(response.data.route.legs[0].steps);
-        })
-
-} catch (error) {
-    console.error(error);
-}
+axios.request(options)
+    .then((response: AxiosResponse) => {
+        console.log(response.data.route.legs[0].steps);
+    })
+    .catch((error: any) => {
+            console.error(error)
+        }
+    )
