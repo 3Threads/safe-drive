b0db16ec320e3364cc5cff34eea39bc5bff6304c
GuRgEnA <agurg21@freeuni.edu.ge>
2023-12-15 16:55:52 +0400

    Initial code to fetch directions

--- safe-drive-back/src/main.ts
+++ safe-drive-back/src/main.ts
@@ -1,1 +1,23 @@
-console.log("asda")
+const axios = require('axios');
+
+const options = {
+    method: 'GET',
+    url: 'https://trueway-directions2.p.rapidapi.com/FindDrivingRoute',
+    params: {
+        stops: '41.6938,44.8015;41.6461,41.6405;'
+    },
+    headers: {
+        'X-RapidAPI-Key': 'e3fc70ca95msh0c1271a45bc037fp13f1eajsn7c9aa6fa0095',
+        'X-RapidAPI-Host': 'trueway-directions2.p.rapidapi.com'
+    }
+};
+
+try {
+    axios.request(options)
+        .then((response: any) =>{
+            console.log(response.data.route.legs[0].steps);
+        })
+
+} catch (error) {
+    console.error(error);
+}
