4367b3a221d4ecdc9dece077fb52c4befa6580a3
GuRgEnA <agurg21@freeuni.edu.ge>
2023-12-15 14:45:22 +0400

    Initial project

---
+++ safe-drive-back/tests/testMain.ts
@@ -0,0 +1,18 @@
+// {
+//     "name": "untitled1",
+//     "version": "1.0.0",
+//     "main": "index.js",
+//     "license": "MIT",
+//     "scripts": {
+//     "build": "cd safe-drive-back && yarn build",
+//         "start-back": "cd safe-drive-back && yarn start"
+// },
+//     "dependencies": {
+//     "typescript": "^5.3.3"
+// },
+//
+//     "workspaces": [
+//     "safe-drive-back",
+//     "safe-drive-front"
+// ]
+// }
