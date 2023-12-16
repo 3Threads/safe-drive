79bf89a39e7bf8d795891a0a00b2c29158f3d25c
DachiGoshadze <dgosh21@freeuni.edu.ge>
2023-12-16 14:51:44 +0400

    add weather

--- safe-drive-back/src/weather.ts
+++ safe-drive-back/src/weather.ts
@@ -1,15 +1,30 @@
-const request = require('request');
-var key = "36590f00de7e4c2b1dda2a979f203aa5";
+import axios, {AxiosResponse} from "axios";
 
-function get_url(lat: number, lon: number) {
-    return `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
-}
 
 export function get_weather(lat: number, lon: number) {
-    let url = get_url(lat, lon);
-    request(url, function(err:any, response:any, body: any){
-        var weather = JSON.parse(body)
-        console.log(weather);
-        return weather;
-    });
+    const option = {
+        method: 'GET',
+        url: "https://api.open-meteo.com/v1/forecast",
+        params: {
+            "latitude": lat,
+            "longitude": lon,
+            "hourly": ["temperature_2m", "apparent_temperature", "precipitation_probability", "precipitation", "rain", "showers", "snowfall", "visibility", "wind_speed_180m", "wind_direction_180m", "temperature_180m"]
+        }
+    };
+    return axios.request(option).then((response: AxiosResponse) => {
+        return response.data;
+    })
+}
+export function get_lat_and_lon(city: string) {
+
+    const option = {
+        method: 'GET',
+        url: `https://api.api-ninjas.com/v1/geocoding?city=${city}`,
+        headers: {
+            'X-Api-Key': '9h9nv0dwprO2gCijEsfnjg==LxoD02CHY72NgISw'
+        },
+    };
+    return axios.request(option).then((response: AxiosResponse) => {
+        return response.data;
+    })
 }
