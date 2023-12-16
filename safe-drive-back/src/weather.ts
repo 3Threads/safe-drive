547c377f007f4339d8a10b488535dd02f370d2d8
DachiGoshadze <dgosh21@freeuni.edu.ge>
2023-12-16 20:47:40 +0400

    add weather interface

--- safe-drive-back/src/weather.ts
+++ safe-drive-back/src/weather.ts
@@ -1,5 +1,6 @@
 import axios, {AxiosResponse} from "axios";
 import {Coordinates} from "./Interfaces/coordinates";
+import {WeatherInterface} from "./Interfaces/weather-interface";
 //
 //
 // export function get_weather(city: string): Promise<any> {
@@ -23,7 +24,7 @@ import {Coordinates} from "./Interfaces/coordinates";
 //         });
 // }
 
-export function getWeatherByCoordinates(coordinate: Coordinates): Promise<any> {
+export function getWeatherByCoordinates(coordinate: Coordinates, date: string, time: string): Promise<any> {
     const option = {
         method: 'GET',
         url: "https://api.open-meteo.com/v1/forecast",
@@ -35,10 +36,44 @@ export function getWeatherByCoordinates(coordinate: Coordinates): Promise<any> {
     };
     return axios.request(option)
         .then((response: AxiosResponse) => {
-            return response.data
+            var d : string = get_full_date(date, time)
+            var weath: WeatherInterface = {
+                apparent_temperature: 0,
+                precipitation: 0,
+                precipitation_probability: 0,
+                rain: 0,
+                showers: 0,
+                snowfall: 0,
+                temperature_180m: 0,
+                temperature_2m: 0,
+                time: "",
+                visibility: 0,
+                wind_direction_180m: 0,
+                wind_speed_180m: 0
+            };
+            for(let i = 0; i<response.data.hourly.time.length; i++) {
+                if(response.data.hourly.time[i] === d) {
+                    weath.time = response.data.hourly.time[i];
+                    weath.apparent_temperature =  response.data.hourly.apparent_temperature[i];
+                    weath.precipitation = response.data.hourly.precipitation[i];
+                    weath.precipitation_probability = response.data.hourly.precipitation_probability[i];
+                    weath.rain = response.data.hourly.rain[i];
+                    weath.showers = response.data.hourly.showers[i];
+                    weath.snowfall = response.data.hourly.snowfall[i];
+                    weath.temperature_180m = response.data.hourly.temperature_180m[i];
+                    weath.temperature_2m =  response.data.hourly.temperature_2m[i];
+                    weath.visibility = response.data.hourly.visibility[i];
+                    weath.wind_direction_180m = response.data.hourly.wind_direction_180m[i];
+                    weath.wind_speed_180m = response.data.hourly.wind_speed_180m[i];
+                    break;
+                }
+            }
+            return weath;
         });
 }
-
+function get_full_date(date: string, time: string) {
+    return date + "T" + time;
+}
 export function getCoordinate(city: string): Promise<Coordinates> {
     const option = {
         method: 'GET',
