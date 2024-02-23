9462612068b9900f8cc04fc5de4d71d1b739cd32
GuRgEnA <agurg21@freeuni.edu.ge>
2024-02-23 15:46:48 +0400

    Adjust zoom sizes

--- src/Components/MapComponents/MapTile.tsx
+++ src/Components/MapComponents/MapTile.tsx
@@ -18,7 +18,7 @@ const MapTile = ({coordinates, releaseDate}: Props) => {
 
     return (
         <div className="App">
-            <MapContainer center={[41.6938, 44.8015]} zoom={13} style={{height: "100vh"}}>
+            <MapContainer center={[41.6938, 44.8015]} zoom={13} style={{height: "100vh"}} maxZoom={17} minZoom={3}>
                 <TileLayer
                     url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
                     attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
